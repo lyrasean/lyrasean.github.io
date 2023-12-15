5f22623e45945f94c6215c8dfc8d104208ec3b27
lyrasean <sean_zhang@foxmail.com>
2023-12-15 14:50:21 +0800

    Update spider.sh

--- live/spider.sh
+++ live/spider.sh
@@ -7,29 +7,14 @@ fi
 touch ~/test.m3u
 echo '#EXTM3U' > ~/test.m3u
 
-num=3221225700
+num=3221225500
 
 while (( $num < 3221227000 ))
 do
-	wget -4 -q -s -T 1 http://39.134.24.162/dbiptv.sn.chinamobile.com/PLTV/88888888/224/${num}/1.m3u8
+	wget -4 -q -s -T 1 http://39.134.24.161/dbiptv.sn.chinamobile.com/PLTV/88888890/224/${num}/index.m3u8
 	if [ $? -eq 0 ]; then
 		echo '#EXTINF:-1 ,'${num} >> ~/test.m3u
-		echo 'http://39.134.24.162/dbiptv.sn.chinamobile.com/PLTV/88888888/224/'${num}'/1.m3u8' >> ~/test.m3u
-	fi
- 	wget -4 -q -s -T 1 http://39.134.24.162/dbiptv.sn.chinamobile.com/PLTV/88888888/224/${num}/index.m3u8
-	if [ $? -eq 0 ]; then
-		echo '#EXTINF:-1 ,'${num} >> ~/test.m3u
-		echo 'http://39.134.24.162/dbiptv.sn.chinamobile.com/PLTV/88888888/224/'${num}'/index.m3u8' >> ~/test.m3u
-	fi
- 	wget -4 -q -s -T 1 http://39.134.24.166/dbiptv.sn.chinamobile.com/PLTV/88888890/224/${num}/1.m3u8
-	if [ $? -eq 0 ]; then
-		echo '#EXTINF:-1 ,'${num} >> ~/test.m3u
-		echo 'http://39.134.24.166/dbiptv.sn.chinamobile.com/PLTV/88888890/224/'${num}'/1.m3u8' >> ~/test.m3u
-	fi
-	wget -4 -q -s -T 1 http://39.134.24.166/dbiptv.sn.chinamobile.com/PLTV/88888890/224/${num}/index.m3u8
-	if [ $? -eq 0 ]; then
-		echo '#EXTINF:-1 ,'${num} >> ~/test.m3u
-		echo 'http://39.134.24.166/dbiptv.sn.chinamobile.com/PLTV/88888890/224/'${num}'/index.m3u8' >> ~/test.m3u
+		echo 'http://39.134.24.161/dbiptv.sn.chinamobile.com/PLTV/88888890/224/'${num}'/index.m3u8' >> ~/test.m3u
 	fi
 	let num++
 done
