df418af4e5932525bffbcb7843dd14edd49a3445
lyrasean <sean_zhang@foxmail.com>
2023-12-15 16:09:33 +0800

    Update spider.sh

--- live/spider.sh
+++ live/spider.sh
@@ -12,7 +12,7 @@ num=3221225700
 while (( $num < 3221227000 ))
 do
 	wget -4 -q -s -T 1 http://39.134.24.162/dbiptv.sn.chinamobile.com/PLTV/88888890/224/${num}/index.m3u8
- 	# ip=39.134.24.[161,162,165,166]; 88888888,88888890,88888893
+ 	# ip=39.134.24.[161,162,165,166]; 888888[88-99]
 	if [ $? -eq 0 ]; then
 		echo '#EXTINF:-1 ,'${num} >> ~/test.m3u
 		echo 'http://39.134.24.162/dbiptv.sn.chinamobile.com/PLTV/88888890/224/'${num}'/index.m3u8' >> ~/test.m3u
