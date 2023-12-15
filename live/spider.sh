95a6c91aebe215a9c721edfdcd30f406447ae296
lyrasean <sean_zhang@foxmail.com>
2023-12-15 15:45:15 +0800

    Update spider.sh

--- live/spider.sh
+++ live/spider.sh
@@ -9,12 +9,13 @@ echo '#EXTM3U' > ~/test.m3u
 
 num=3221225500
 
-while (( $num < 3221227000 ))
+while (( $num < 3221227500 ))
 do
-	wget -4 -q -s -T 1 http://39.134.24.161/dbiptv.sn.chinamobile.com/PLTV/88888890/224/${num}/index.m3u8
+	wget -4 -q -s -T 1 http://39.134.24.162/dbiptv.sn.chinamobile.com/PLTV/88888890/224/${num}/index.m3u8
+ 	# ip=39.134.24.[161,162,165,166]; 88888888,88888890,88888893
 	if [ $? -eq 0 ]; then
 		echo '#EXTINF:-1 ,'${num} >> ~/test.m3u
-		echo 'http://39.134.24.161/dbiptv.sn.chinamobile.com/PLTV/88888890/224/'${num}'/index.m3u8' >> ~/test.m3u
+		echo 'http://39.134.24.162/dbiptv.sn.chinamobile.com/PLTV/88888890/224/'${num}'/index.m3u8' >> ~/test.m3u
 	fi
 	let num++
 done
