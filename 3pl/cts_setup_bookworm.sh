2c13e6e26981f05ca06df3c7bf92f750df69d7d1
lyrasean <sean_zhang@foxmail.com>
2024-01-16 17:13:37 +0800

    Update cts_setup_bookworm.sh

--- 3pl/cts_setup_bookworm.sh
+++ 3pl/cts_setup_bookworm.sh
@@ -37,7 +37,7 @@ function MUST_RUN_FIRST()
 	echo -e "${BLUE}Installing building prerequisites...${RESET}"
 	echo $password | sudo -S apt install -y git-core gnupg flex bison build-essential zip curl \
  	zlib1g-dev libc6-dev-i386 libncurses5 x11proto-core-dev libx11-dev lib32z1-dev \
-  	libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig wget gedit
+  	libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig wget gedit ffmpeg
 
 	# Installing python3.11-full. bookworm has installed Python 3.11.2
 	echo -e "${BLUE}Installing Python 3.11 and tools...${RESET}"
