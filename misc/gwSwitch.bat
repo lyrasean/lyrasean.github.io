dabc1cdbf1ccd6c46d11631a0c42e618f6cedcf6
lyrasean <sean_zhang@foxmail.com>
2024-02-02 10:07:53 +0800

    Update gwSwitch.bat

--- misc/gwSwitch.bat
+++ misc/gwSwitch.bat
@@ -1,5 +1,11 @@
 @echo off
 
+REM Double-click
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+goto is_admin
+
+REM Run_as check
 net session 1>nul 2>&1 && (
     goto is_admin
 ) || (
