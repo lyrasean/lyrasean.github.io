540acf1a174ad2e95647e7123af060f2d2083f89
lyrasean <sean_zhang@foxmail.com>
2024-09-01 12:23:44 +0800

    Create getinfo.bat

---
+++ nxt/getinfo.bat
@@ -0,0 +1,19 @@
+@echo off
+
+md c:\nxt
+pwsh -NoProfile -ExecutionPolicy Bypass -File "getinfo.ps1"
+
+:: Get installed drivers
+Dism /online /get-drivers > c:\nxt\drivers.txt
+
+:: Check if any issue for driver
+pnputil /enum-devices /problem
+pnputil /enum-devices /problem > c:\nxt\device_problem.txt
+
+:: Get driver status
+pnputil /export-pnpstate c:\nxt\pnpstate.pnp
+echo Use pnpexplr to open pnpstate.pnp
+
+systeminfo > c:\nxt\sysinfo.txt
+
+echo list vol | diskpart
