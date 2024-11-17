e548e7487066f2850d85de99464b1714db6afdd9
lyrasean <sean_zhang@foxmail.com>
2024-11-17 10:10:47 +0800

    Update getinfo.bat

--- nxt/getinfo.bat
+++ nxt/getinfo.bat
@@ -5,12 +5,15 @@ pwsh -NoProfile -ExecutionPolicy Bypass -File "getinfo.ps1"
 
 :: Get installed drivers
 Dism /online /get-drivers > c:\nxt\drivers.txt
+md c:\nxt\backup
+pnputil /export-driver oem1.inf c:\nxt\backup
+pnputil /export-driver * c:\nxt\backup
 
-:: Check if any issue for driver
+:: Check if device with problem exists
 pnputil /enum-devices /problem
 pnputil /enum-devices /problem > c:\nxt\device_problem.txt
 
-:: Get driver status
+:: Get device/driver status
 pnputil /export-pnpstate c:\nxt\pnpstate.pnp
 echo Use pnpexplr to open pnpstate.pnp
 
